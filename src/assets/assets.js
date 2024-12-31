a91450a8112a1233cfb2eed5d559aa3eafacf56c
stan <moon_saga@yahoo.com.tw>
2024-12-31 12:24:14 +1100

    react and tailwind website

---
+++ src/assets/assets.js
@@ -0,0 +1,67 @@
+import home_icon from './home.png'
+import search_icon from './search.png'
+import stack_icon from './stack.png'
+import arrow_icon from './arrow.png'
+import plus_icon from './plus.png'
+import img1 from './song1.webp'
+import shuffle_icon from './shuffle.png'
+import prev_icon from './prev.png'
+import play_icon from './play.png'
+import next_icon from './next.png'
+import loop_icon from './loop.png'
+import plays_icon from './plays.png'
+import mic_icon from './mic.png'
+import queue_icon from './queue.png'
+import speaker_icon from './speaker.png'
+import volume_icon from './volume.png'
+import mini_icon from './mini.png'
+import zoom_icon from './zoom.png'
+
+export const assets={
+    home_icon,
+    search_icon,
+    stack_icon,
+    arrow_icon,
+    plus_icon,
+    shuffle_icon,
+    prev_icon,
+    play_icon,
+    next_icon,
+    loop_icon,
+    plays_icon,
+    mic_icon,
+    queue_icon,
+    speaker_icon,
+    volume_icon,
+    mini_icon,
+    zoom_icon,
+}
+
+export const albumsdata=[
+    {
+        id:0,
+        name:"Top 50 Global",
+        //image: im8,
+        desc:"Your weekly update of the most played tracks",
+        bgcolor:"#2a4365"
+    },
+    {
+        id:1,
+        name:"Top 50 India",
+        //image:im9,
+        desc:"Your weekly update of the most played tracks",
+        bgcolor:"#22543d"
+        
+    },
+]
+
+export const songsdata=[
+    {
+        id:0,
+        name:"Song one",
+        image:img1,
+        //file:song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:00"
+    },
+]
---
+++ src/component/player.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {songsdata, assets} from '../assets/assets'
+
+const Player = () => {
+  return (
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+        <div className='lg:flex items-center gap-4'>
+            <img className='w-12' src={songsdata[0].image} alt="first song"/>
+            <div>
+                <p>{songsdata[0].name}</p>
+                <p>{songsdata[0].desc.slice(0,12)}</p>
+            </div>
+        </div>
+        <div className='flex flex-col items-center gap-1 m-auto'>
+            <div className="flex gap-4">
+                <img className='w-6 cursor-pointer' src={assets.shuffle_icon} alt="shuffle"/>
+                <img className='w-6 cursor-pointer' src={assets.prev_icon} alt="prev"/>
+                <img className='w-6 cursor-pointer' src={assets.play_icon} alt="play"/>
+                <img className='w-6 cursor-pointer' src={assets.next_icon} alt="loop"/>
+                <img className='w-6 cursor-pointer' src={assets.loop_icon} alt=""/>
+            </div>
+            <div className="flex items-center gap-5">
+                <p>1:06</p>
+                <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+                    <hr className='h-1 border-none w-0 bg-blue-800 rounded-full'/>
+                </div>
+                <p>3:20</p>
+            </div>
+        </div>
+        <div className='hidden lg:flex items-center gap-2 opacity-75'>
+            <img className='w-6' src={assets.plays_icon} alt='play'/>
+            <img className='w-6' src={assets.mic_icon} alt='mic'/>
+            <img className='w-6' src={assets.queue_icon} alt='queue'/>
+            <img className='w-6' src={assets.speaker_icon} alt='speaker'/>
+            <img className='w-6' src={assets.volume_icon} alt='volume'/>
+            <div className="w-20 bg-slate-50 h-1 rounded">
+                
+            </div>
+            <img className='w-6' src={assets.mini_icon} alt='mini'/>
+            <img className='w-6' src={assets.zoom_icon} alt='zoom'/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Player
