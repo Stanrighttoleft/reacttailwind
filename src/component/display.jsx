3e372622f10dc0f4a60686a4f77d7d82f322eaae
stan <moon_saga@yahoo.com.tw>
2025-01-01 16:21:04 +1100

    route test

--- src/component/display.jsx
+++ src/component/display.jsx
@@ -7,7 +7,7 @@ const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
-            <Route path='./' element={<Displayhome/>}/>
+            <Route path='https://stanrighttoleft.github.io/' element={<Displayhome/>}/>
         </Routes>
 
     </div>
