a3f5400f90a3dc39f43e29c1c0a5f7ac45a6a889
stan <moon_saga@yahoo.com.tw>
2025-01-01 15:47:00 +1100

    album data

--- src/component/displayhome.jsx
+++ src/component/displayhome.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './navbar'
+import {albumsdata} from '../assets/assets'
+import Albumitem from './albumitem'
 
 const Displayhome = () => {
   return (
     <>
     <Navbar/>
+    <div className='my-5 font-bold text-2xl'>
+        {albumsdata.map((item,index)=>(<Albumitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+    </div>
     
     </>
    
