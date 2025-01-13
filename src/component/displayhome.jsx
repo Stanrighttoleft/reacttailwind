5487d077c7b3ce24e357723e347caae784b084a6
stan <moon_saga@yahoo.com.tw>
2025-01-13 18:16:56 +1100

    today biggest hit added

--- src/component/displayhome.jsx
+++ src/component/displayhome.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './navbar'
 import {albumsdata} from '../assets/assets'
 import Albumitem from './albumitem'
+import { songsdata } from '../assets/assets'
+import Songitem from './songitem'
 
 const Displayhome = () => {
   return (
@@ -9,11 +11,17 @@ const Displayhome = () => {
     <Navbar/>
     <div className='mb-4'>
       <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
-      <div className="flex overflow-auto">
+      <div className='flex overflow-auto'>
         {albumsdata.map((item,index)=>(<Albumitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))} 
 
-      </div>
-        
+      </div>     
+    </div>
+    <div className='mb-4'>
+      <h1 className='my-5 font-bold text-2xl'>Today Biggest Hit</h1>
+      <div className='flex overflow-auto'>
+        {songsdata.map((item,index)=>(<Songitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))} 
+
+      </div>     
     </div>
     
     </>
